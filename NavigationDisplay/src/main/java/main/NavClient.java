e2a3d4fb8473a660de384a0f4db5752a85cfda3b
gjwo <grahamjwood@gmail.com>
2016-11-17 16:03:25 +0000

    working pom, client waits for stream but fails csv recognition

--- NavigationDisplay/src/main/java/main/NavClient.java
+++ NavigationDisplay/src/main/java/main/NavClient.java
@@ -6,7 +6,11 @@ import java.util.regex.Pattern;
 
 public class NavClient {
 	private static final int serverPortNbr = 9876;
-    private static final Pattern dataTFFFsplit = Pattern.compile("(\\d+),([+-]?\\d*\\.?\\d*),([+-]?\\d*\\.?\\d*),([+-]?\\d*\\.?\\d*)");
+    private static final Pattern dataTFFFsplit = Pattern.compile(	"[+-]([0-9]+),"
+    																+ "[+-]([0-9]*[.])?[0-9]+,"
+    																+ "[+-]([0-9]*[.])?[0-9]+,"
+    																+ "[+-]([0-9]*[.])?[0-9]+,");
+    private static final int debugLevel = 4;
 
 	public NavClient() {
 		// TODO Auto-generated constructor stub
@@ -17,8 +21,6 @@ public class NavClient {
              System.out.println("Usage: java NavClient <hostname>");
              return;
         }
- 
-            // get a datagram socket
         DatagramSocket socket = new DatagramSocket();
  
             // send request
@@ -27,37 +29,66 @@ public class NavClient {
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, serverPortNbr);
         socket.send(packet); //register interest with server
         boolean stop = false;
-        Pattern msgSplit = Pattern.compile("(\\p{Alpha}+),(?<=sentence).*");
+        Pattern msgSplit = Pattern.compile("(^.*?),(.*$)");
         Matcher msg;
         String msgType;
+    	DatagramPacket inPacket = new DatagramPacket(buf, buf.length);
+    	socket.setSoTimeout(500); //1/2 second timeout
+    	boolean reply = false;
+    	while(!reply) //keep trying to register until we get a response
+    	{
+    		reply = true;
+    		try
+    		{
+                socket.receive(inPacket);
+    			
+    		}catch (SocketTimeoutException e) {
+    		       	// resend
+    				reply = false;
+    				socket.send(packet);
+    		}
+    	}
+    	//we lose the first response
+    	socket.setSoTimeout(0); //clear timeout
         while (!stop)
         {
             // get response
-        	DatagramPacket inPacket = new DatagramPacket(buf, buf.length);
-            socket.receive(inPacket);
- 
+        	inPacket = new DatagramPacket(buf, buf.length);
+            socket.receive(inPacket); //now wait for reply
+
             // display response
             String received = new String(inPacket.getData(), 0, packet.getLength());
+            if(debugLevel<=5) System.out.println("DEBUG main received: "+received);
             msg = msgSplit.matcher(received);
-            msgType = msg.group(1);
-            switch (msgType)
-            {
-            case "STOP": 	stop = true;
-            				break;
-            case "Angles":	processAnglesMsg(msg.group(2));
-            				break;
-            default:		System.out.println("msg: " + received);
-            }   
+            if (msg.matches())
+	            {
+	            msgType = msg.group(1);
+	            switch (msgType)
+	            {
+	            case "STOP": 	stop = true;
+	            				break;
+	            case "Angles":	processAnglesMsg(msg.group(2));
+	            				break;
+	            default:		System.out.println("msg: " + received);
+	            }
+            }
         }
+        System.out.println("Stopping");
         socket.close();
     }
     private static void processAnglesMsg(String s)
     {
     	Matcher data = dataTFFFsplit.matcher(s);
-    	Long time = Long.parseLong(data.group(1));
-    	float yaw = Float.parseFloat(data.group(2));
-    	float pitch = Float.parseFloat(data.group(3));
-    	float roll = Float.parseFloat(data.group(4));
-    	System.out.format("Angles - [%8d] Yaw: %08.3f Pitch: %08.3f Roll: %08.3f%n",time,yaw, pitch,roll);
+    	if(data.matches())
+    	{
+	    	Long time = Long.parseLong(data.group(1));
+	    	float yaw = Float.parseFloat(data.group(2));
+	    	float pitch = Float.parseFloat(data.group(3));
+	    	float roll = Float.parseFloat(data.group(4));
+	    	System.out.format("Angles - [%8d] Yaw: %08.3f Pitch: %08.3f Roll: %08.3f%n",time,yaw, pitch,roll);
+    	} else
+    	{
+    		if(debugLevel<=4) System.out.format("DEBUG processAnglesMsg: "+s);
+    	}
     }
 }
