d0856dc70621c73c5c2b021fdfbfe05d46417e18
MAWood <mattcow12@googlemail.com>
2016-12-27 12:56:33 +0000

    Added log level filtering to the log display

--- NavigationDisplay/src/main/java/org/ladbury/mainGUI/elements/SwingLogDisplay.java
+++ NavigationDisplay/src/main/java/org/ladbury/mainGUI/elements/SwingLogDisplay.java
@@ -5,6 +5,8 @@ import logging.RemoteLog;
 import org.ladbury.userInterfacePkg.UiStyle;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.rmi.NotBoundException;
@@ -16,7 +18,7 @@ import java.util.ArrayList;
  * NavigationDisplay - org.ladbury.userInterfacePkg
  * Created by MAWood on 26/12/2016.
  */
-public class SwingLogDisplay extends JTextArea implements Runnable
+public class SwingLogDisplay extends JPanel implements Runnable, ChangeListener
 {
     private final Thread thread;
 
@@ -25,26 +27,61 @@ public class SwingLogDisplay extends JTextArea implements Runnable
     private int count;
     private final Registry registry;
 
+    private final JTextArea textArea;
+    private final JSpinner levelSpinner;
+    private int viewingLevel;
+
     public SwingLogDisplay(Registry registry)
     {
         super();
+        this.setLayout(new BorderLayout());
+
         this.registry = registry;
-        this.setText("");
-        this.setBackground(Color.pink);
-        this.setColumns(80);
-        this.setCursor(null);
-        this.setEditable(false);
-        this.setFont(UiStyle.NORMAL_FONT);
-        this.setRows(20);
-        DefaultCaret caret = (DefaultCaret)this.getCaret();
-        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        textArea = new JTextArea();
+        levelSpinner = new JSpinner();
+
+
+        configureTextArea();
+        configureLevelSelector();
 
         this.count = 0;
-
+        this.viewingLevel = 2;
         this.localEntries = new ArrayList<>();
 
         thread = new Thread(this);
         thread.start();
+    }
+
+    private void configureLevelSelector()
+    {
+        JPanel spinnerPanel = new JPanel();
+        SpinnerModel model =
+                new SpinnerNumberModel(2,  //initial value
+                                    1,  //min
+                                    10, //max
+                                     1); //step
+        levelSpinner.setModel(model);
+        levelSpinner.addChangeListener(this);
+        spinnerPanel.setLayout(new BorderLayout());
+        spinnerPanel.add(new JLabel("Viewing Level:"), BorderLayout.WEST);
+        spinnerPanel.add(levelSpinner, BorderLayout.EAST);
+        JPanel paddingPanel = new JPanel(new BorderLayout());
+        paddingPanel.add(spinnerPanel, BorderLayout.WEST);
+        this.add(paddingPanel, BorderLayout.SOUTH);
+    }
+
+    private void configureTextArea()
+    {
+        textArea.setText("");
+        textArea.setBackground(Color.pink);
+        textArea.setColumns(80);
+        textArea.setCursor(null);
+        textArea.setEditable(false);
+        textArea.setFont(UiStyle.NORMAL_FONT);
+        textArea.setRows(20);
+        DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        this.add(textArea, BorderLayout.CENTER);
     }
 
     @Override
@@ -77,14 +114,22 @@ public class SwingLogDisplay extends JTextArea implements Runnable
         String text = "";
         for(LogEntry entry:localEntries)
         {
+            if(entry.level.getLevel() > viewingLevel) continue;
             text += entry.toString();
             text += System.lineSeparator();
         }
-        this.setText(text);
+        textArea.setText(text);
     }
 
     public void stop()
     {
         thread.interrupt();
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e)
+    {
+        viewingLevel = (int)levelSpinner.getValue();
+        updateText();
     }
 }
