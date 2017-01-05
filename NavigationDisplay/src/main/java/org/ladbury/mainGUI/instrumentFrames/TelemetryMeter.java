2c0b984e5670620daa72a458fa5e2105735966e3
gjwo <grahamjwood@gmail.com>
2017-01-05 17:04:41 +0000

    reversible meter intervals (coloured zones) more setters

--- NavigationDisplay/src/main/java/org/ladbury/mainGUI/instrumentFrames/TelemetryMeter.java
+++ NavigationDisplay/src/main/java/org/ladbury/mainGUI/instrumentFrames/TelemetryMeter.java
@@ -48,6 +48,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.text.DecimalFormat;
 import java.util.EnumSet;
 import java.util.concurrent.TimeUnit;
 
@@ -72,7 +73,7 @@ public class TelemetryMeter extends SubSystemDependentJFrame implements Runnable
 	private static final long serialVersionUID = 1585778226074987267L;
 	private final DefaultValueDataset dataset;
 	private JFreeChart chart;
-	private final ChartPanel chartPanel;
+	private ChartPanel chartPanel;
 
     private Thread thread;
     private RemoteTelemetry telemetry;
@@ -84,6 +85,8 @@ public class TelemetryMeter extends SubSystemDependentJFrame implements Runnable
     private double scaleNormalEnd;
     private double scaleWarningEnd;
     private double scaleCriticalEnd;
+    private String name;
+    private String units; 
  
 	/**
      * TelemetryMeter	-	Constructor
@@ -91,8 +94,10 @@ public class TelemetryMeter extends SubSystemDependentJFrame implements Runnable
     public TelemetryMeter()
     {
         super(EnumSet.of(SubSystem.SubSystemType.TELEMETRY));
-        this.setTitle("Meter");
-        setScale(0,15);   	
+        this.name = "Meter";
+        this.setTitle(this.name);
+        this.units = "Volts";
+        setScale(0,15,false);   	
         dataset = new DefaultValueDataset(new Double(0.0)); //Create the dataset (single value)
         chart = createChart(dataset);	//Create the chart
         
@@ -125,18 +130,58 @@ public class TelemetryMeter extends SubSystemDependentJFrame implements Runnable
         this.setVisible(true);
     }
 
-    public void setScale(double start, double end)
+    public void setScale(double start, double end, boolean CriticalHigh)
     {
-    	scaleStart = start;
-    	scaleEnd = end;
-        scaleNormalStart = scaleStart;
-        scaleWarningStart = scaleStart + (scaleEnd-scaleStart)*0.7;
-        scaleCriticalStart = scaleStart + (scaleEnd-scaleStart)*0.9;
-        scaleNormalEnd = scaleWarningStart;
-        scaleWarningEnd = scaleCriticalStart;
-        scaleCriticalEnd = scaleEnd;
+    	if (CriticalHigh)
+    	{
+	    	scaleStart = start;
+	    	scaleEnd = end;
+	        scaleNormalStart = scaleStart;
+	        scaleWarningStart = scaleStart + (scaleEnd-scaleStart)*0.7;
+	        scaleCriticalStart = scaleStart + (scaleEnd-scaleStart)*0.9;
+	        scaleNormalEnd = scaleWarningStart;
+	        scaleWarningEnd = scaleCriticalStart;
+	        scaleCriticalEnd = scaleEnd;
+    	} else
+    	{
+	    	scaleStart = start;
+	    	scaleEnd = end;
+	        scaleNormalStart = (scaleEnd-scaleStart)*0.8;
+	        scaleWarningStart = scaleStart + (scaleEnd-scaleStart)*0.7;
+	        scaleCriticalStart = scaleStart;
+	        scaleNormalEnd = scaleEnd;
+	        scaleWarningEnd = scaleStart + (scaleEnd-scaleStart)*0.8;
+	        scaleCriticalEnd = scaleStart + (scaleEnd-scaleStart)*0.7;
+
+    	}
+    		
+    }
+    public boolean setCriticalScale(double start, double end)
+    {
+    	if ((start < scaleStart) | (start > scaleEnd) |(end < scaleEnd) |(end > scaleEnd)) return false;
+    	this.scaleCriticalStart = start;
+    	this.scaleCriticalEnd = end;
+    	return true;
     }
     
+    public boolean setWarningScale(double start, double end)
+    {
+    	if ((start < scaleStart) | (start > scaleEnd) |(end < scaleEnd) |(end > scaleEnd)) return false;
+    	this.scaleWarningStart = start;
+    	this.scaleWarningEnd = end;
+    	return true;
+    }
+    
+    public boolean setNormalScale(double start, double end)
+    {
+    	if ((start < scaleStart) | (start > scaleEnd) |(end < scaleEnd) |(end > scaleEnd)) return false;
+    	this.scaleNormalStart = start;
+    	this.scaleNormalEnd = end;
+    	return true;
+    }
+    
+    public void setName(String name){ this.name = name;}
+    public void setUnits(String units){this.units = units;}
     @Override
     public void run()
     {
@@ -160,7 +205,7 @@ public class TelemetryMeter extends SubSystemDependentJFrame implements Runnable
     	MeterPlot plot = new MeterPlot(dataset);
     	DialShape shape = DialShape.CIRCLE;
     	
-    	plot.setUnits("Volts");
+    	plot.setUnits(this.units);
     	plot.setRange(new Range(scaleStart, scaleEnd));
     	plot.addInterval(new MeterInterval("Normal", new Range(scaleNormalStart, scaleNormalEnd), Color.lightGray, new BasicStroke(2.0f),  new Color(0, 255, 0, 64)));
     	plot.addInterval(new MeterInterval("Warning", new Range(scaleWarningStart, scaleWarningEnd), Color.lightGray, new BasicStroke(2.0f), new  Color(255, 255, 0, 64)));
@@ -169,12 +214,24 @@ public class TelemetryMeter extends SubSystemDependentJFrame implements Runnable
     	plot.setDialShape(shape);
      	plot.setNeedlePaint(Color.white);
     	plot.setTickLabelFont(new Font("SansSerif", Font.BOLD, 9));
+    	plot.setTickLabelFormat(new DecimalFormat("0"));
+    	plot.setTickLabelPaint(Color.white);
+    	plot.setTickSize(1.0);
+    	plot.setTickLabelsVisible(true);
     	plot.setInsets(new RectangleInsets(5, 5, 5, 5));
-    	JFreeChart chart = new JFreeChart(	"Meter Chart", 
+    	JFreeChart chart = new JFreeChart(	this.name, 
     			 							JFreeChart.DEFAULT_TITLE_FONT, 
     			 							plot, 
     			 							false);
         return chart;
+    }
+    public void recreateChart()
+    {
+    	this.chart = createChart(this.dataset);
+    	this.chartPanel = new ChartPanel(this.chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(300, 270));
+        chartPanel.setEnforceFileExtensions(false);
+        setContentPane(chartPanel); //add the panel to the ApplicationFrame
     }
     
     public void setValue(double d) {System.out.println(d+"V");this.dataset.setValue(d);}
